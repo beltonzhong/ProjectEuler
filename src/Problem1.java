c5f2cf4f6a0c32204172eb60b5c3a6a1a02dad2b
Belton Zhong <byzhong@ucsd.edu>
2014-12-25 21:05:41 -0800

    Problem 1
    Multiples of 3 and 5

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,5 +1,17 @@
-/**
- * Created by Belton on 12/22/2014.
- */
+import java.util.Scanner;
+
 public class Problem1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int numInputs = input.nextInt();
+    for(int i = 0; i < numInputs; i++)
+      System.out.println(multiplesThreeFive(input.nextInt()));
+  }
+
+  private static int multiplesThreeFive(int number) {
+    int sumThrees = (3 + number / 3 * 3) * (number / 6);
+    int sumFives = (5 + number / 5 * 5) * (number / 5);
+    int sumFifteen = (15 + number / 15 * 15 ) * (number / 15);
+    return sumThrees + sumFives - sumFifteen;
+  }
 }
